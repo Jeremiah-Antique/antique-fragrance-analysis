82c4f0b0b26f258302fd47f94cdf28c514a9eed7
Jeremiah-Antique <158342396+Jeremiah-Antique@users.noreply.github.com>
2024-02-09 13:58:35 +0000

    Added to personal and average functions for increased user options (added name as condition for plotting data)

--- Hello.py
+++ Hello.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import streamlit as st, pandas as pd
+import streamlit as st, pandas as pd, plotly.express as px
 
 from streamlit.logger import get_logger
 
@@ -60,7 +60,27 @@ def P_Average_HTS(df_name):
 
   data = reformat(data)
   return data
+#function to calculate average HTS for Each Door
+def D_Average_HTS(df_name):
+    # Group the data by 'Name' and 'Fragrance'.
+  grouped_df = df_name.groupby(['Door', 'Fragrance'])
 
+  # Calculate the average 'HTS' for each grouping of 'Door' and 'Fragrance'.
+  average_hts = grouped_df['HTS'].mean()
+
+  # Create a new DataFrame with the results.
+  data = pd.DataFrame({'Door': average_hts.index.get_level_values(0),
+                       'Fragrance': average_hts.index.get_level_values(1),
+                       'Average_HTS': average_hts})
+  def reformat(df_name):
+      #rename columns, then drop them and reset index to clean up df
+      df_name = df_name.rename(columns={'Fragrance': 'Frag', 'Door': 'Door'})
+      df_name = df_name.drop(['Door', 'Frag'], axis=1)
+      df_name = df_name.reset_index()
+      return df_name
+
+  data = reformat(data)
+  return data
 #function to calculate average HTS and group by whatever column you call for
 def Average_HTS(df_name,name):
         Avg_HTS = df_name[df_name['Fragrance'].str.strip() == name]['HTS'].mean()
@@ -69,11 +89,24 @@ def Average_HTS(df_name,name):
 def run_Personal_average():
     Personal = P_Average_HTS(New_excel)
     st.write("Running Personal HT Average")
-    st. write(Personal)
+    HT_Name = pd.DataFrame()
+    HT_Name = Personal[Personal['Fragrance']==name]
+    st. write(HT_Name)
+    fig= px.bar(HT_Name, x = 'Name', y= 'Average_HTS',title='Personal HT Scores')
+    st.plotly_chart(fig)
 def run_HT_average():
     HT_average = Average_HTS(New_excel,name)
     st.write("Running HT Average")
     st. write(HT_average)
+
+def run_door_average():
+    HT_average = D_Average_HTS(New_excel)
+    st.write("Running Door HT Average")
+    HT_Name = pd.DataFrame()
+    HT_Name = HT_average[HT_average['Fragrance']==name]
+    st. write(HT_Name)
+    fig= px.bar(HT_Name, x = 'Door', y= 'Average_HTS',title= 'HT Scores by Door')
+    st.plotly_chart(fig)
 
 #run the function
 
@@ -111,16 +144,26 @@ if uploaded_file is not None:
         st.write(New_excel)
 
     st.title("Run Hot Throw Analysis")
-#Buttoms to run functions for Personal and Fragrance Hot throw
-    if st.button("Fragrance Hot Throw Average"):
-        HT_AVG = Average_HTS(New_excel,name)
-        st.write("Hot Throw Average for", name,"is",HT_AVG)
-
-# create button to run Personal Average
-    if st.button("Personal Hot Throw Average"):
-        run_Personal_average()
-else: st.write("Upload your Excel Document to Start")
-
+#Buttons to run functions for Personal and Fragrance Hot throw
+    st.header("How many HT rooms were used in this test?")
+    doors = st.select_slider("Number of HT Rooms used",options=(1,2,3))
+    if doors == 1:
+        if st.button("Fragrance Hot Throw Average"):
+            HT_AVG = Average_HTS(New_excel,name)
+            st.write("Hot Throw Average for", name,"is",HT_AVG)
+    else:
+#Buttons to run functions for Personal and Fragrance Hot throw
+        if st.button("Door Hot Throw Average"):
+            run_door_average()
+    st.header("Do you have more than 1 HT test in your data for a single fragrance?")
+    yes = st.checkbox("Yes",key = "yes_checkbox")
+    no = st.checkbox("No")
+    if yes:
+        # create button to run Personal Average
+        if st.button("Personal Hot Throw Average"):
+            run_Personal_average()
+    
+else: st.write("Upload your Excel Document to Start")  
 
 
 
